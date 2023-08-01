3a8f8a3eb2917bee7f3a1f48b48f39196a1f0be9
Jon <jprather.dev@gmail.com>
2023-08-01 14:17:36 -0700

    added basic video player

---
+++ src/lib/components/+YoutubePlayer.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	export let videoUrl = '';
+	function convertYouTubeUrl(url: string) {
+		if (url.includes('youtu.be')) {
+			const videoId = url.split('/').pop();
+			return `https://www.youtube.com/embed/${videoId}`;
+		} else {
+			return url;
+		}
+	}
+	const embeddableUrl = convertYouTubeUrl(videoUrl);
+</script>
+
+<div class="video-responsive">
+	<iframe
+		title="Video Player"
+		width="853"
+		height="480"
+		src={embeddableUrl}
+		frameborder="0"
+		allowfullscreen
+	/>
+</div>
+
+<style>
+	.video-responsive {
+		position: relative;
+		padding-bottom: 56.25%; /* Aspect ratio */
+		height: 0;
+		overflow: hidden;
+		object-position: cover;
+	}
+
+	.video-responsive iframe {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- src/routes/movies/[slug]/+page.svelte
+++ src/routes/movies/[slug]/+page.svelte
@@ -3,46 +3,60 @@
 	import { page } from '$app/stores';
 	import type { MovieDetail } from '../+layout.server';
 	import Breadcrumbs from '$lib/components/+Breadcrumbs.svelte';
+	import YoutubePlayer from '$lib/components/+YoutubePlayer.svelte';
 
 	export let data: PageData;
-	let film = data.films.find((film) => $page.params.slug.startsWith(String(film.id)));
+	let filmId = Number($page.params.slug.split('-')[0]);
+	let film = data.films.find((film) => film.id === filmId);
+	// TODO this is overeager needs to match not just the start ie 36-blade
+	// it gets the 3 and goes to iron man 2 (which is id 3) very jankily
 
 	const { release_date, phase, duration, chronology } = film as MovieDetail;
 
 	let stats = [
 		// { title: 'release_date', value: release_date },
+		{ title: 'runtime', value: duration ? duration + ' min' : 'N/A' },
 		{ title: 'phase', value: phase },
-		{ title: 'runtime', value: duration + ' min' },
 		{ title: 'chronology', value: chronology }
+
 		// { title: 'runtime', value: runtime }
 	];
 </script>
 
 <Breadcrumbs path={$page.url.pathname} />
 {#if film}
-	<div class="hero min-h-screen bg-base-200 rounded">
+	<div class="hero min-h-screen bg-slate-800 rounded">
 		<div class="hero-content flex-col lg:flex-row">
-			<img alt="MCU cover" src={film.cover_url} class="max-w-sm rounded-lg shadow-2xl" />
+			<img alt="MCU cover" src={film.cover_url} class="sm:max-w-sm rounded-lg shadow-2xl" />
 			<div class="p-4">
 				<h1 class="text-5xl font-bold">{film.title}</h1>
-				<p class="py-6">
-					{film.overview}
+
+				<p class="py-6 max-w-[70ch]">
+					{film.overview ?? 'N/A'}
 				</p>
 
 				<div class="stats stats-vertical lg:stats-horizontal shadow mt-10">
 					{#each stats as { title, value }}
 						<div class="stat">
 							<div class="stat-title capitalize">{title}</div>
-							<div class="stat-value">{value}</div>
+							<div class="stat-value">{value ?? 'N/A'}</div>
 						</div>
 					{/each}
 				</div>
 
 				<div class="stat mt-10">
 					<div class="stat-title capitalize">Release date</div>
-					<div class="stat-value">{film.release_date}</div>
+					<div class="stat-value">{film.release_date ?? 'N/A'}</div>
 				</div>
 			</div>
 		</div>
 	</div>
+	{#if film?.trailer_url}
+		<div class="pt-10 bg-slate-900">
+			<h2 class=" text-center text-4xl font-bold pt-10 pb-4">Release Trailer</h2>
+			<div class=" mx-auto p-2 sm:p-10 sm:max-w-[70%]">
+				<YoutubePlayer videoUrl={film.trailer_url} />
+			</div>
+		</div>
+	{/if}
 {/if}
