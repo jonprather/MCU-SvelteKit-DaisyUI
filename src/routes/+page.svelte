629347a8b3c89f9735ea0293e29c3167bd633930
Jon <jprather.dev@gmail.com>
2023-08-01 14:16:01 -0700

    added basic home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,16 @@
 	export let data: PageData;
 </script>
 
-<Breadcrumbs path={$page.url.pathname} />
-<div class=" m-auto pt-20" />
+<div class=" m-auto">
+	<Breadcrumbs path={$page.url.pathname} />
+
+	<div class="hero min-h-screen bg-slate-800">
+		<div class="hero-content text-center">
+			<div class="max-w-md">
+				<h1 class="text-5xl font-bold">MCU</h1>
+				<p class="py-6">A marvel Comic Universe Website.</p>
+				<a class="btn btn-primary" href="/movies">Movies</a>
+			</div>
+		</div>
+	</div>
+</div>
