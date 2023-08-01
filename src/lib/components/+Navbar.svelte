e32e6ef610d917e16092fc9fac971789f2debb63
Jon <jprather.dev@gmail.com>
2023-08-01 14:08:56 -0700

    added basic nav bar

---
+++ src/lib/components/+Navbar.svelte
@@ -0,0 +1,50 @@
+<div class="navbar bg-base-100">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<button class="btn btn-ghost btn-circle" aria-label="Open menu">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h7"
+					/></svg
+				>
+			</button>
+			<div
+				tabindex="0"
+				class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+			>
+				<ul role="menu">
+					<li role="menuitem"><a href="/">Home</a></li>
+					<li role="menuitem"><a href="/movies">Movies</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="navbar-center">
+		<a href="/" class="btn btn-ghost normal-case text-xl">MCU</a>
+	</div>
+	<div class="navbar-end">
+		<button class="btn btn-ghost btn-circle" aria-label="Search">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+				/></svg
+			>
+		</button>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
 	import '../app.css';
+	import Navbar from '$lib/components/+Navbar.svelte';
 
 	export let data: LayoutData;
 </script>
+
+<Navbar />
 
 <slot />
